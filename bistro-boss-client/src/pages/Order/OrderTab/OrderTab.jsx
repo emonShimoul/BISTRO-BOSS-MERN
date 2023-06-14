296911f7ec18e944009fb5825936fd23a626fb4f
emonShimoul <emonshimoul@gmail.com>
2023-06-15 00:21:53 +0600

    adding work with pagination

--- bistro-boss-client/src/pages/Order/OrderTab/OrderTab.jsx
+++ bistro-boss-client/src/pages/Order/OrderTab/OrderTab.jsx
@@ -1,11 +1,33 @@
 import FoodCart from "../../../components/FoodCart/FoodCart";
+import { Swiper, SwiperSlide } from "swiper/react";
+import { Pagination } from "swiper";
+
+// Import Swiper styles
+import "swiper/css";
+import "swiper/css/pagination";
 
 const OrderTab = ({ items }) => {
+  const pagination = {
+    clickable: true,
+    renderBullet: function (index, className) {
+      return '<span class="' + className + '">' + (index + 1) + "</span>";
+    },
+  };
   return (
-    <div className="grid md:grid-cols-3 gap-10">
-      {items.map((item) => (
-        <FoodCart key={item._id} item={item}></FoodCart>
-      ))}
+    <div>
+      <Swiper
+        pagination={pagination}
+        modules={[Pagination]}
+        className="mySwiper"
+      >
+        <SwiperSlide>
+          <div className="grid md:grid-cols-3 gap-10">
+            {items.map((item) => (
+              <FoodCart key={item._id} item={item}></FoodCart>
+            ))}
+          </div>
+        </SwiperSlide>
+      </Swiper>
     </div>
   );
 };

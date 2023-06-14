73abd9219d4cbd11d5cacd790de1ec4ab573081d
emonShimoul <emonshimoul@gmail.com>
2023-06-14 21:31:57 +0600

    display different menu category items with cover

--- bistro-boss-client/src/pages/Menu/Menu/Menu.jsx
+++ bistro-boss-client/src/pages/Menu/Menu/Menu.jsx
@@ -1,14 +1,46 @@
 import { Helmet } from "react-helmet-async";
 import Cover from "../../Shared/Cover/Cover";
 import menuImg from "../../../assets/menu/banner3.jpg";
+import dessertImg from "../../../assets/menu/dessert-bg.jpeg";
+import pizzaImg from "../../../assets/menu/pizza-bg.jpg";
+import saladImg from "../../../assets/menu/salad-bg.jpg";
+import soupImg from "../../../assets/menu/soup-bg.jpg";
+import useMenu from "../../../hooks/useMenu";
+import SectionTitle from "../../../components/SectionTitle/SectionTitle";
+import MenuCategory from "../MenuCategory/MenuCategory";
 
 const Menu = () => {
+  const [menu] = useMenu();
+  const offered = menu.filter((item) => item.category === "offered");
+  const desserts = menu.filter((item) => item.category === "dessert");
+  const soup = menu.filter((item) => item.category === "soup");
+  const salad = menu.filter((item) => item.category === "salad");
+  const pizza = menu.filter((item) => item.category === "pizza");
+
   return (
     <div>
       <Helmet>
         <title>Bistro Boss | Menu</title>
       </Helmet>
       <Cover img={menuImg} title="Our Menu"></Cover>
+      {/* main cover */}
+      <SectionTitle
+        subHeading="Don't Miss"
+        heading="Today's Offer"
+      ></SectionTitle>
+      {/* offered menu items */}
+      <MenuCategory items={offered}></MenuCategory>
+      {/* dessert menu items */}
+      <MenuCategory
+        items={desserts}
+        title="Desserts"
+        img={dessertImg}
+      ></MenuCategory>
+
+      <MenuCategory items={pizza} title="Pizza" img={pizzaImg}></MenuCategory>
+
+      <MenuCategory items={salad} title="Salad" img={saladImg}></MenuCategory>
+      <MenuCategory items={soup} title="Salad" img={soupImg}></MenuCategory>
     </div>
   );
 };

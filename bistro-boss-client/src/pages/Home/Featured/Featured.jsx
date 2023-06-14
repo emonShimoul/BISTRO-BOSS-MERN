cae73f3a5d15347d66ac2d26c479cc3a5e3a831f
emonShimoul <emonshimoul@gmail.com>
2023-06-14 13:51:12 +0600

    create parallax and add testimonials with react rating

--- bistro-boss-client/src/pages/Home/Featured/Featured.jsx
+++ bistro-boss-client/src/pages/Home/Featured/Featured.jsx
@@ -4,12 +4,12 @@ import "./Featured.css";
 
 const Featured = () => {
   return (
-    <div className="featured-item text-white pt-8 my-20">
+    <div className="featured-item bg-fixed text-white pt-8 my-20 ">
       <SectionTitle
         subHeading="check it out"
         heading="Featured Item"
       ></SectionTitle>
-      <div className="md:flex justify-center items-center pb-20 pt-12 px-36">
+      <div className="md:flex justify-center items-center pb-20 pt-12 px-36 bg-slate-500 bg-opacity-60 ">
         <div>
           <img src={featuredImg} alt="" />
         </div>
@@ -24,7 +24,9 @@ const Featured = () => {
             Iure, ipsam! Molestias laboriosam dolorum dolorem. Adipisci eveniet
             doloribus aperiam recusandae eum voluptates! Dolorum.
           </p>
-          <button className="btn btn-outline">Order Now</button>
+          <button className="btn btn-outline border-0 border-b-4 mt-4">
+            Order Now
+          </button>
         </div>
       </div>
     </div>

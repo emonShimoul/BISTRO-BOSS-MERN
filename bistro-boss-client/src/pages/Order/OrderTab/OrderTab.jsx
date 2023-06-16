444fe40136d70473b9469333356dbc936f3a532d
emonShimoul <emonshimoul@gmail.com>
2023-06-16 17:51:24 +0600

    Add to Cart

--- bistro-boss-client/src/pages/Order/OrderTab/OrderTab.jsx
+++ bistro-boss-client/src/pages/Order/OrderTab/OrderTab.jsx
@@ -6,6 +6,7 @@ import { Pagination } from "swiper";
 import "swiper/css";
 import "swiper/css/pagination";
 
+// TODO: Implement pagination here on this page
 const OrderTab = ({ items }) => {
   const pagination = {
     clickable: true,

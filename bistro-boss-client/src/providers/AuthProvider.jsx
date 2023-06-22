81f257683daaf20d1460a9e43682545af44d93a1
emonShimoul <emonshimoul@gmail.com>
2023-06-23 01:33:03 +0600

    create a jwt token to back end for verification

--- bistro-boss-client/src/providers/AuthProvider.jsx
+++ bistro-boss-client/src/providers/AuthProvider.jsx
@@ -10,6 +10,7 @@ import {
   updateProfile,
 } from "firebase/auth";
 import { app } from "../firebase/firebase.config";
+import axios from "axios";
 
 export const AuthContext = createContext(null);
 
@@ -53,6 +54,18 @@ const AuthProvider = ({ children }) => {
       setUser(currentUser);
       console.log(currentUser);
       setLoading(false);
+
+      // get and set token
+      if (currentUser) {
+        axios
+          .post("http://localhost:5000/jwt", { email: currentUser.email })
+          .then((data) => {
+            // console.log(data.data.token);
+            localStorage.setItem("access-token", data.data.token);
+          });
+      } else {
+        localStorage.removeItem("access-token");
+      }
     });
 
     return () => {
